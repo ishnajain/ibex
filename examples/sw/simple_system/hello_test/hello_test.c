abc81add2a906720393e233cc167272e2f5ce98c
ishnaxd <ishnaxd@gmail.com>
2024-04-03 00:01:32 +0530

    basic setup

--- examples/sw/simple_system/hello_test/hello_test.c
+++ examples/sw/simple_system/hello_test/hello_test.c
@@ -4,38 +4,55 @@
 
 #include "simple_system_common.h"
 
+// void print1(){
+//     puts("from Print1\n");
+
+// }
+// void print2(){
+//     puts("From print2\n");
+
+// }
+
+
+
 int main(int argc, char **argv) {
-  pcount_enable(0);
-  pcount_reset();
-  pcount_enable(1);
+  // pcount_enable(0);
+  // pcount_reset();
+  // pcount_enable(1);
 
-  puts("Hello simple system\n");
-  puthex(0xDEADBEEF);
-  putchar('\n');
-  puthex(0xBAADF00D);
-  putchar('\n');
+  // puts("Hello simple system\n");
+  // puthex(0xDEADBEEF);
+  // putchar('\n');
+  // puthex(0xBAADF00D);
+  // putchar('\n');
+  // print1();
+  // print2();
 
-  pcount_enable(0);
+  puts("from Print1\n");
+  // putc("s");
+ 
+ 
+  // pcount_enable(0);
 
   // Enable periodic timer interrupt
   // (the actual timebase is a bit meaningless in simulation)
-  timer_enable(2000);
+  // timer_enable(2000);
 
-  uint64_t last_elapsed_time = get_elapsed_time();
+  // uint64_t last_elapsed_time = get_elapsed_time();
 
-  while (last_elapsed_time <= 4) {
-    uint64_t cur_time = get_elapsed_time();
-    if (cur_time != last_elapsed_time) {
-      last_elapsed_time = cur_time;
+  // while (last_elapsed_time <= 4) {
+  //   uint64_t cur_time = get_elapsed_time();
+  //   if (cur_time != last_elapsed_time) {
+  //     last_elapsed_time = cur_time;
 
-      if (last_elapsed_time & 1) {
-        puts("Tick!\n");
-      } else {
-        puts("Tock!\n");
-      }
-    }
-    asm volatile("wfi");
-  }
+  //     if (last_elapsed_time & 1) {
+  //       puts("Tick!\n");
+  //     } else {
+  //       puts("Tock!\n");
+  //     }
+  //   }
+  //   asm volatile("wfi");
+  // }
 
   return 0;
 }
---
+++ lowrisc-toolchain-gcc-rv32imcb-20240206-1/lib/gcc/riscv32-unknown-elf/10.2.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/__w/lowrisc-toolchains/lowrisc-toolchains/build/gcc/.build/riscv32-unknown-elf/src/gcc/configure --build=x86_64-build_pc-linux-gnu --host=x86_64-build_pc-linux-gnu --target=riscv32-unknown-elf --prefix=/tools/riscv --exec_prefix=/tools/riscv --with-local-prefix=/tools/riscv/riscv32-unknown-elf --with-headers=/tools/riscv/riscv32-unknown-elf/include --with-newlib --enable-threads=no --disable-shared --with-arch=rv32imc --with-abi=ilp32 --with-pkgversion='crosstool-NG 1.26.0_rc1' --with-bugurl=toolchains@lowrisc.org --enable-__cxa_atexit --disable-tm-clone-registry --disable-libgomp --disable-libmudflap --disable-libmpx --disable-libssp --disable-libquadmath --disable-libquadmath-support --disable-libstdcxx-verbose --with-gmp=/__w/lowrisc-toolchains/lowrisc-toolchains/build/gcc/.build/riscv32-unknown-elf/buildtools --with-mpfr=/__w/lowrisc-toolchains/lowrisc-toolchains/build/gcc/.build/riscv32-unknown-elf/buildtools --with-mpc=/__w/lowrisc-toolchains/lowrisc-toolchains/build/gcc/.build/riscv32-unknown-elf/buildtools --with-isl=/__w/lowrisc-toolchains/lowrisc-toolchains/build/gcc/.build/riscv32-unknown-elf/buildtools --enable-lto --enable-target-optspace --without-long-double-128 --disable-nls --disable-multilib --enable-languages=c,c++";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "abi", "ilp32" }, { "arch", "rv32imc" } };
